9bdfcdfea07a4bb37fcb3a02535b746c52915022
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2020-11-03 15:26:21 -0500

    Add in premede mix cards

---
+++ src/components/IngredientCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Card, Image, Form, Checkbox, Input} from "semantic-ui-react";
+import style from "./IngredientCard.module.css";
+
+export default class IngredientCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+
+        this.state = {
+            checked: false
+        }
+    }
+
+    onClick(e) {
+        this.setState({checked: !this.state.checked});
+    }
+
+    render() {
+        return (
+            <Card onClick={this.onClick} style={{backgroundColor: this.state.checked ? "#00cc66" : "", transition: "background-color 1s"}}>
+                <Image src={this.props.imageSrc} size={"small"} centered/>
+                <Card.Content value={this.props.name}>
+                    <Card.Header>{this.props.name}</Card.Header>
+                    <Card.Description>
+                        {this.props.price}
+                    </Card.Description>
+                    <Form.Field style={{display: "none"}} control={Input} type={"checkbox"} name={this.props.name} checked={this.state.checked ? "checked" : ""} />
+                </Card.Content>
+            </Card>
+        )
+    }
+}
--- src/pages/order.js
+++ src/pages/order.js
@@ -1,6 +1,7 @@
 import React from "react"
 import style from "./order.module.css";
 import isEmail from 'validator/lib/isEmail';
+import IngredientCard from "../components/IngredientCard";
 import proddata from "../data"
 import {
     Button,
@@ -10,7 +11,7 @@ import {
     Radio,
     Select,
     TextArea,
-    Message, Container
+    Message, Container, Segment
 } from "semantic-ui-react"
 import 'semantic-ui-css/semantic.min.css'
 import encodeURL from "encodeurl"
@@ -26,6 +27,8 @@ export default class Order extends React.Component{
         this.validateData = this.validateData.bind(this);
         this.onChange = this.onChange.bind(this);
         this.getMailURL = this.getMailURL.bind(this);
+        this.renderChooseMenu = this.renderChooseMenu.bind(this);
+        this.renderPresetMenu = this.renderPresetMenu.bind(this);
 
         this.state = {
             success: false,
@@ -33,6 +36,8 @@ export default class Order extends React.Component{
             error: false,
             errorMessage: "Unexpected error",
             price: 0,
+            presetMenu: false,
+            chooseMenu: false,
             data: {
                 name: "",
                 email: ""
@@ -51,6 +56,14 @@ export default class Order extends React.Component{
         }
 
         return 0;
+    }
+
+    renderChooseMenu(e) {
+        this.setState({presetMenu: false, chooseMenu: true});
+    }
+
+    renderPresetMenu(e) {
+        this.setState({presetMenu: true, chooseMenu: false});
     }
 
     onChange(e, {name, value}) {
@@ -78,9 +91,9 @@ export default class Order extends React.Component{
         return (
             <Layout>
                 <Header size={"huge"}>Order Form</Header>
-                <iframe width={0} height={0} frameBorder={0}  title={"dummyframe"} name={"dummyframe"} id={"dummyframe"}/>
+                <Message warning>Please be advised to not consume this product if you have an allergy to <strong>wheat/gluten, milk, eggs, peanuts, tree nuts, or soy.</strong> </Message>
                 <p>Please fill out some personal details before you order. Please acknowledge that we currently accept
-                    cash only, which will be paid on pickup of trail mix. </p>
+                    cash only, which will be paid on pickup of trail mix. <strong>To learn about the health precautions we take for preparing your food, please look <a href={"https://docs.google.com/document/d/1Ev2AldUpJc0sHFMCgKsRu3AdWRRLrvBjPJAlPM_rUUE/edit?usp=sharing"}>here</a></strong>.</p>
                 <Form className={style.form} target={"dummyframe"} onSubmit={this.onSubmit}
                       action={this.submitURL} method={"POST"} success={this.state.success} error={this.state.error}>
                     <Form.Group widths='equal'>
@@ -99,14 +112,47 @@ export default class Order extends React.Component{
                             onChange={this.onChange}
                         />
                     </Form.Group>
+                    {/* Hidden Fields*/}
+                    <Form.Field
+                        control={Input}
+                        name={"status"}
+                        value={"PENDING"}
+                        type={"hidden"}
+                    />
+                    <br />
+
+                    <Message attached={"top"}>
+                        Choose an ordering option:
+                    </Message>
+
+                    <Button.Group fluid size={"large"} attached={"bottom"}>
+                        <Button type={"button"} onClick={this.renderPresetMenu}>Pre-made</Button>
+                        <Button.Or />
+                        <Button color={"yellow"} type={"button"} onClick={this.renderChooseMenu}>Choose your own</Button>
+                    </Button.Group>
+
+                    {/* Choose Menu */}
+                    <Segment color={"yellow"} style={{display: this.state.chooseMenu ? "" : "none"}}>
+                        <p>Stinky</p>
+                    </Segment>
+
+                    {/* Preset Menu */}
+                    <Segment color={"grey"} style={{display: this.state.presetMenu ? "" : "none"}}>
+                        <IngredientCard imageSrc={"https://www.bulkbarn.ca/app_themes/BulkBarn/Images/assets/products/full/23_000023.png"} name={"Baha"} price={"$69"} />
+                    </Segment>
+
+                    <br />
                     <Message header={"Total"} content={"$" + this.state.price.toFixed(2)} attached={true}/>
                     <Message size={"small"} attached={true}>
                         <p>Please check all details before ordering. To cancel an order, please email <a href={this.getMailURL()}>thetrailmixfactory@gmailcom</a>.</p>
                     </Message>
                     <Message success header={'Order Completed!'}  content={'Your order was submitted!'} />
                     <Message error header={'Error'} content={this.state.errorMessage} />
+
                     <br />
-                    <Form.Field control={Button} floated={"right"} type={"submit"} active>Submit</Form.Field>
+                    <Form.Field control={Button} size={"large"} floated={"right"} type={"submit"} active>Submit</Form.Field>
+
+                    <iframe style={{width: 0, height: 0}} width={0} height={0} frameBorder={0} title={"dummyframe"} name={"dummyframe"} id={"dummyframe"}/>
                 </Form>
             </Layout>
         );
